9a6c81d4981f4ad0c3e6fd6b6ca224f59414ab87
Vita Kyrdan <v.kyrdan@gmail.com>
2020-04-16 21:35:00 +0300

    my first commit:)

---
+++ find_the_number/script.js
@@ -0,0 +1,22 @@
+function getRandomInt(arg1) {
+  return Math.floor(Math.random() * arg1);
+}
+let range = prompt("Select range");
+range = parseInt(range);
+let guessNumber = getRandomInt(range);
+
+while (true) {
+	let findNumber = prompt("Write a umber");
+	findNumber = parseInt(findNumber);
+	if (!findNumber) {
+		alert("Go on, without 'Zero' and 'Cancel'");
+		continue;
+	}
+	if (findNumber === guessNumber) {
+		alert("Congratulation!"); 
+	} else if (findNumber > guessNumber) {
+		alert("Your number is bigger that guessNumber, Try gain");
+	} else { 
+		alert("Your number is smaller that guessNumber, Try gainn");
+	}
+}
